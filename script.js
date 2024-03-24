d96b6efc2903cfe8b27ac7f85c45449426a2214f
Antony Petsas <152808269+TKP888@users.noreply.github.com>
2024-03-24 16:41:52 +0000

    javascript added and code cleaned up
    issue with submit since js added, need to fix

---
+++ script.js
@@ -0,0 +1,23 @@
+const passInputOne = document.querySelector("#password");
+const passInputTwo = document.querySelector("#password_confirm");
+const form = document.querySelector("#form");
+const errorLabel = document.querySelector("#errorText");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const passOne = passInputOne.value;
+    const passTwo = passInputTwo.value;
+
+    if (form.checkValidity()) {
+        if (passOne === passTwo && passOne !== "") {
+            errorLabel.innerHTML = "Thank you for signing up";
+        } else {
+            errorLabel.innerHTML = "* Passwords do not match";
+            passInputOne.classList.add("error");
+            passInputTwo.classList.add("error");
+        }
+    } else {
+        form.reportValidity();
+    }
+});
